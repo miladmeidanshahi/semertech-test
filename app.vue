c6ceb63bedef130c3819dcc9bb4a4ca2eb156964
Milad Meidanshahi <{ID}+{username}@users.noreply.github.com>
2024-02-24 19:55:38 +0330

    added default layout

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout dir="rtl">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <app-header />
+
+    <div class="container mx-auto">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+html
+  background-color: #E9EDF5
+</style>
