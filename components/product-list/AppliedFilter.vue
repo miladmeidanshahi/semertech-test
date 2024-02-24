27c5cdc4e3233d064fbf0eb80f0bd93091884761
Milad Meidanshahi <{ID}+{username}@users.noreply.github.com>
2024-02-24 19:56:06 +0330

    added product list with cmps

---
+++ components/product-list/AppliedFilter.vue
@@ -0,0 +1,8 @@
+<template>
+  <u-card class="rounded-3xl mb-6">
+    <div class="flex">
+      <div class="text-grey-900 text-sm">فیلتر های اعمال شده</div>
+      <div></div>
+    </div>
+  </u-card>
+</template>
---
+++ pages/product-list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="py-6">
+    <product-list-applied-filter />
+    <div class="grid grid-cols-3 gap-x-4 gap-y-6">
+      <div
+        v-for="product in products"
+        :key="product.id"
+      >
+        <product-list-card v-bind="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'default'
+})
+
+const { pending, data: products } = await useFetch('https://fakestoreapi.com/products')
+</script>
