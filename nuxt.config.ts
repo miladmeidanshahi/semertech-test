fc381ad1a98f961869907e3f47fb1df2fffdd6e1
Milad Meidanshahi <{ID}+{username}@users.noreply.github.com>
2024-02-26 20:04:22 +0330

    added font and styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
   modules: ['@nuxt/ui', "@nuxt/image"],
   colorMode: {
     preference: 'light'
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: '@use "~/assets/css/styles.sass" as *\n'
+        }
+      }
+    }
   }
 })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,33 @@ import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
   theme: {
+    fontFamily: {
+      sans: [
+        'iransansxv',
+        {
+          fontFeatureSettings: '"ss02"',
+          fontVariationSettings: '"dots" 8'
+        }
+      ]
+    },
     extend: {
       colors: {
+        red: {
+          50: '#FCE5EE',
+          200: '#F180A9'
+        },
         blue: {
           900: '#0A2A51'
         },
         gray: {
-          50: '#E9EDF5'
+          50: '#E9EDF5',
+          500: '#647E9A',
+          700: '#445A74',
+          900: '#253343'
         },
-        rose: '#E20054'
+        rose: {
+          500: '#E20054'
+        }
       },
       aspectRatio: {
         auto: 'auto',
