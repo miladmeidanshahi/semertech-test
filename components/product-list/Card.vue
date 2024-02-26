f093740d53c1cb9d43de5e57b47c8a3ac37315f7
Milad Meidanshahi <{ID}+{username}@users.noreply.github.com>
2024-02-26 20:06:12 +0330

    update product style  card

--- components/product-list/Card.vue
+++ components/product-list/Card.vue
@@ -1,37 +1,48 @@
 <template>
   <u-card :ui="CardConfig">
-    <template #header>
-      <nuxt-img
-        :src="$attrs.image"
-        class="w-full rounded-2xl object-contain"
-      />
-    </template>
+    <nuxt-img
+      :src="$attrs.image"
+      class="w-full rounded-2xl object-contain"
+    />
 
     <div
       :title="$attrs.title"
+      class="text-blue-900 text-sm font-bold truncate ... my-4"
       v-text="$attrs.title"
-      class="text-blue-900 text-sm font-bold truncate ..."
     />
 
-    <template #footer>
-      <u-button
-        size="lg"
-        class="border-slate-50"
-        color="white"
-        block
-        :ui="{ rounded: 'rounded-2xl' }"
+    <u-button
+      size="lg"
+      class="border-slate-50"
+      color="white"
+      block
+      :ui="{ rounded: 'rounded-2xl' }"
+    >
+      <span class="text-rose-500">مشاهده جزئیات</span>
+      <svg
+        width="17"
+        height="16"
+        viewBox="0 0 17 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <span class="text-rose">مشاهده جزئیات</span>
-        <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10.8332 13.28L6.48656 8.9333C5.97323 8.41997 5.97323 7.57997 6.48656 7.06664L10.8332 2.71997" stroke="#E20054" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-      </u-button>
-    </template>
+        <path
+          d="M10.8332 13.28L6.48656 8.9333C5.97323 8.41997 5.97323 7.57997 6.48656 7.06664L10.8332 2.71997"
+          stroke="#E20054"
+          stroke-width="1.5"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </u-button>
   </u-card>
 </template>
 
 <script setup>
 const CardConfig = {
+  body: { padding: 'p-3' },
+  header: { padding: 'p-3' },
   divide: '',
   rounded: 'rounded-3xl'
 }
@@ -39,5 +50,5 @@ const CardConfig = {
 
 <style scoped lang="sass">
 img
-  aspect-ratio: 16/6
+  aspect-ratio: 16/11
 </style>
