93da29a432dc7fcdd046e701d4cb3a77062e5c8f
Milad Meidanshahi <{ID}+{username}@users.noreply.github.com>
2024-02-27 18:38:16 +0330

    added skeleton

--- pages/product-list.vue
+++ pages/product-list.vue
@@ -7,12 +7,26 @@
       </div>
       <div class="basis-full md:basis-3/4">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-6">
-          <div
-            v-for="product in products"
-            :key="product.id"
-          >
-            <product-list-card v-bind="product" />
-          </div>
+          <!-- this is for example to see skeleton -->
+          <template v-if="loading">
+            <u-card
+              v-for="n in 6"
+              :key="n"
+            >
+              <u-skeleton class="w-full aspect-square" />
+              <u-skeleton class="h-4 my-4" />
+              <u-skeleton class="rounded-2xl h-10" />
+            </u-card>
+          </template>
+
+          <template v-else>
+            <div
+              v-for="product in products"
+              :key="product.id"
+            >
+              <product-list-card v-bind="product" />
+            </div>
+          </template>
         </div>
       </div>
     </div>
@@ -20,13 +34,17 @@
 </template>
 
 <script setup>
-const { products, getProducts } = useProductService()
-
 definePageMeta({
   layout: 'default'
 })
 
+const { products, getProducts } = useProductService()
+
+const loading = ref(true)
+
 const { data } = await getProducts()
 
 products.value = data.value
+
+setTimeout(() => (loading.value = false), 1000)
 </script>
